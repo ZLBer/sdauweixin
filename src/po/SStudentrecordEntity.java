f78c0f5e75566c6c3ce98cf4b38375617d0afe4c
”平行时空git config --global user.email <1604030622@qq.com>
2018-04-19 21:53:17 +0800

    新增数据库

---
+++ src/po/SStudentrecordEntity.java
@@ -0,0 +1,479 @@
+package po;
+
+/**
+ * @author 平行时空
+ * @created 2018-04-19 21:41
+ **/
+public class SStudentrecordEntity {
+    private String sId;
+    private String sGraduationyear;
+    private String sName;
+    private String sIdentitycard;
+    private String sSex;
+    private String sNation;
+    private String sPoliticaloutlook;
+    private String sEducation;
+    private String sMajor;
+    private String sCulturemode;
+    private String sBirthplace;
+    private String sDepartment;
+    private String sClass;
+    private int sStudylength;
+    private String sForeignlanguages;
+    private String sGraduating;
+    private String sSendout;
+    private String sDispatchstate;
+    private String sHomephone;
+    private String sMyphone;
+    private String sReceivingunit;
+    private String sSign;
+    private String sForaddress;
+    private String sTrandepartment;
+    private String sUnittranloaction;
+    private String sReporttype;
+    private String sUnitreceivtype;
+    private String sGraaddress;
+    private String sUnitcategorycode;
+    private String sUnitnature;
+    private String sUnitorgcode;
+    private String sUnittype;
+    private String sUnitcomName;
+    private String sUnitinformation;
+    private String sFlecontphone;
+    private String sUnitslocation;
+    private String sFlesalary;
+    private Integer sIsemployment;
+    private int sIschanged;
+    private String sChangedhistory;
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getsGraduationyear() {
+        return sGraduationyear;
+    }
+
+    public void setsGraduationyear(String sGraduationyear) {
+        this.sGraduationyear = sGraduationyear;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsIdentitycard() {
+        return sIdentitycard;
+    }
+
+    public void setsIdentitycard(String sIdentitycard) {
+        this.sIdentitycard = sIdentitycard;
+    }
+
+    public String getsSex() {
+        return sSex;
+    }
+
+    public void setsSex(String sSex) {
+        this.sSex = sSex;
+    }
+
+    public String getsNation() {
+        return sNation;
+    }
+
+    public void setsNation(String sNation) {
+        this.sNation = sNation;
+    }
+
+    public String getsPoliticaloutlook() {
+        return sPoliticaloutlook;
+    }
+
+    public void setsPoliticaloutlook(String sPoliticaloutlook) {
+        this.sPoliticaloutlook = sPoliticaloutlook;
+    }
+
+    public String getsEducation() {
+        return sEducation;
+    }
+
+    public void setsEducation(String sEducation) {
+        this.sEducation = sEducation;
+    }
+
+    public String getsMajor() {
+        return sMajor;
+    }
+
+    public void setsMajor(String sMajor) {
+        this.sMajor = sMajor;
+    }
+
+    public String getsCulturemode() {
+        return sCulturemode;
+    }
+
+    public void setsCulturemode(String sCulturemode) {
+        this.sCulturemode = sCulturemode;
+    }
+
+    public String getsBirthplace() {
+        return sBirthplace;
+    }
+
+    public void setsBirthplace(String sBirthplace) {
+        this.sBirthplace = sBirthplace;
+    }
+
+    public String getsDepartment() {
+        return sDepartment;
+    }
+
+    public void setsDepartment(String sDepartment) {
+        this.sDepartment = sDepartment;
+    }
+
+    public String getsClass() {
+        return sClass;
+    }
+
+    public void setsClass(String sClass) {
+        this.sClass = sClass;
+    }
+
+    public int getsStudylength() {
+        return sStudylength;
+    }
+
+    public void setsStudylength(int sStudylength) {
+        this.sStudylength = sStudylength;
+    }
+
+    public String getsForeignlanguages() {
+        return sForeignlanguages;
+    }
+
+    public void setsForeignlanguages(String sForeignlanguages) {
+        this.sForeignlanguages = sForeignlanguages;
+    }
+
+    public String getsGraduating() {
+        return sGraduating;
+    }
+
+    public void setsGraduating(String sGraduating) {
+        this.sGraduating = sGraduating;
+    }
+
+    public String getsSendout() {
+        return sSendout;
+    }
+
+    public void setsSendout(String sSendout) {
+        this.sSendout = sSendout;
+    }
+
+    public String getsDispatchstate() {
+        return sDispatchstate;
+    }
+
+    public void setsDispatchstate(String sDispatchstate) {
+        this.sDispatchstate = sDispatchstate;
+    }
+
+    public String getsHomephone() {
+        return sHomephone;
+    }
+
+    public void setsHomephone(String sHomephone) {
+        this.sHomephone = sHomephone;
+    }
+
+    public String getsMyphone() {
+        return sMyphone;
+    }
+
+    public void setsMyphone(String sMyphone) {
+        this.sMyphone = sMyphone;
+    }
+
+    public String getsReceivingunit() {
+        return sReceivingunit;
+    }
+
+    public void setsReceivingunit(String sReceivingunit) {
+        this.sReceivingunit = sReceivingunit;
+    }
+
+    public String getsSign() {
+        return sSign;
+    }
+
+    public void setsSign(String sSign) {
+        this.sSign = sSign;
+    }
+
+    public String getsForaddress() {
+        return sForaddress;
+    }
+
+    public void setsForaddress(String sForaddress) {
+        this.sForaddress = sForaddress;
+    }
+
+    public String getsTrandepartment() {
+        return sTrandepartment;
+    }
+
+    public void setsTrandepartment(String sTrandepartment) {
+        this.sTrandepartment = sTrandepartment;
+    }
+
+    public String getsUnittranloaction() {
+        return sUnittranloaction;
+    }
+
+    public void setsUnittranloaction(String sUnittranloaction) {
+        this.sUnittranloaction = sUnittranloaction;
+    }
+
+    public String getsReporttype() {
+        return sReporttype;
+    }
+
+    public void setsReporttype(String sReporttype) {
+        this.sReporttype = sReporttype;
+    }
+
+    public String getsUnitreceivtype() {
+        return sUnitreceivtype;
+    }
+
+    public void setsUnitreceivtype(String sUnitreceivtype) {
+        this.sUnitreceivtype = sUnitreceivtype;
+    }
+
+    public String getsGraaddress() {
+        return sGraaddress;
+    }
+
+    public void setsGraaddress(String sGraaddress) {
+        this.sGraaddress = sGraaddress;
+    }
+
+    public String getsUnitcategorycode() {
+        return sUnitcategorycode;
+    }
+
+    public void setsUnitcategorycode(String sUnitcategorycode) {
+        this.sUnitcategorycode = sUnitcategorycode;
+    }
+
+    public String getsUnitnature() {
+        return sUnitnature;
+    }
+
+    public void setsUnitnature(String sUnitnature) {
+        this.sUnitnature = sUnitnature;
+    }
+
+    public String getsUnitorgcode() {
+        return sUnitorgcode;
+    }
+
+    public void setsUnitorgcode(String sUnitorgcode) {
+        this.sUnitorgcode = sUnitorgcode;
+    }
+
+    public String getsUnittype() {
+        return sUnittype;
+    }
+
+    public void setsUnittype(String sUnittype) {
+        this.sUnittype = sUnittype;
+    }
+
+    public String getsUnitcomName() {
+        return sUnitcomName;
+    }
+
+    public void setsUnitcomName(String sUnitcomName) {
+        this.sUnitcomName = sUnitcomName;
+    }
+
+    public String getsUnitinformation() {
+        return sUnitinformation;
+    }
+
+    public void setsUnitinformation(String sUnitinformation) {
+        this.sUnitinformation = sUnitinformation;
+    }
+
+    public String getsFlecontphone() {
+        return sFlecontphone;
+    }
+
+    public void setsFlecontphone(String sFlecontphone) {
+        this.sFlecontphone = sFlecontphone;
+    }
+
+    public String getsUnitslocation() {
+        return sUnitslocation;
+    }
+
+    public void setsUnitslocation(String sUnitslocation) {
+        this.sUnitslocation = sUnitslocation;
+    }
+
+    public String getsFlesalary() {
+        return sFlesalary;
+    }
+
+    public void setsFlesalary(String sFlesalary) {
+        this.sFlesalary = sFlesalary;
+    }
+
+    public Integer getsIsemployment() {
+        return sIsemployment;
+    }
+
+    public void setsIsemployment(Integer sIsemployment) {
+        this.sIsemployment = sIsemployment;
+    }
+
+    public int getsIschanged() {
+        return sIschanged;
+    }
+
+    public void setsIschanged(int sIschanged) {
+        this.sIschanged = sIschanged;
+    }
+
+    public String getsChangedhistory() {
+        return sChangedhistory;
+    }
+
+    public void setsChangedhistory(String sChangedhistory) {
+        this.sChangedhistory = sChangedhistory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SStudentrecordEntity that = (SStudentrecordEntity) o;
+
+        if (sStudylength != that.sStudylength) return false;
+        if (sIschanged != that.sIschanged) return false;
+        if (sId != null ? !sId.equals(that.sId) : that.sId != null) return false;
+        if (sGraduationyear != null ? !sGraduationyear.equals(that.sGraduationyear) : that.sGraduationyear != null)
+            return false;
+        if (sName != null ? !sName.equals(that.sName) : that.sName != null) return false;
+        if (sIdentitycard != null ? !sIdentitycard.equals(that.sIdentitycard) : that.sIdentitycard != null)
+            return false;
+        if (sSex != null ? !sSex.equals(that.sSex) : that.sSex != null) return false;
+        if (sNation != null ? !sNation.equals(that.sNation) : that.sNation != null) return false;
+        if (sPoliticaloutlook != null ? !sPoliticaloutlook.equals(that.sPoliticaloutlook) : that.sPoliticaloutlook != null)
+            return false;
+        if (sEducation != null ? !sEducation.equals(that.sEducation) : that.sEducation != null) return false;
+        if (sMajor != null ? !sMajor.equals(that.sMajor) : that.sMajor != null) return false;
+        if (sCulturemode != null ? !sCulturemode.equals(that.sCulturemode) : that.sCulturemode != null) return false;
+        if (sBirthplace != null ? !sBirthplace.equals(that.sBirthplace) : that.sBirthplace != null) return false;
+        if (sDepartment != null ? !sDepartment.equals(that.sDepartment) : that.sDepartment != null) return false;
+        if (sClass != null ? !sClass.equals(that.sClass) : that.sClass != null) return false;
+        if (sForeignlanguages != null ? !sForeignlanguages.equals(that.sForeignlanguages) : that.sForeignlanguages != null)
+            return false;
+        if (sGraduating != null ? !sGraduating.equals(that.sGraduating) : that.sGraduating != null) return false;
+        if (sSendout != null ? !sSendout.equals(that.sSendout) : that.sSendout != null) return false;
+        if (sDispatchstate != null ? !sDispatchstate.equals(that.sDispatchstate) : that.sDispatchstate != null)
+            return false;
+        if (sHomephone != null ? !sHomephone.equals(that.sHomephone) : that.sHomephone != null) return false;
+        if (sMyphone != null ? !sMyphone.equals(that.sMyphone) : that.sMyphone != null) return false;
+        if (sReceivingunit != null ? !sReceivingunit.equals(that.sReceivingunit) : that.sReceivingunit != null)
+            return false;
+        if (sSign != null ? !sSign.equals(that.sSign) : that.sSign != null) return false;
+        if (sForaddress != null ? !sForaddress.equals(that.sForaddress) : that.sForaddress != null) return false;
+        if (sTrandepartment != null ? !sTrandepartment.equals(that.sTrandepartment) : that.sTrandepartment != null)
+            return false;
+        if (sUnittranloaction != null ? !sUnittranloaction.equals(that.sUnittranloaction) : that.sUnittranloaction != null)
+            return false;
+        if (sReporttype != null ? !sReporttype.equals(that.sReporttype) : that.sReporttype != null) return false;
+        if (sUnitreceivtype != null ? !sUnitreceivtype.equals(that.sUnitreceivtype) : that.sUnitreceivtype != null)
+            return false;
+        if (sGraaddress != null ? !sGraaddress.equals(that.sGraaddress) : that.sGraaddress != null) return false;
+        if (sUnitcategorycode != null ? !sUnitcategorycode.equals(that.sUnitcategorycode) : that.sUnitcategorycode != null)
+            return false;
+        if (sUnitnature != null ? !sUnitnature.equals(that.sUnitnature) : that.sUnitnature != null) return false;
+        if (sUnitorgcode != null ? !sUnitorgcode.equals(that.sUnitorgcode) : that.sUnitorgcode != null) return false;
+        if (sUnittype != null ? !sUnittype.equals(that.sUnittype) : that.sUnittype != null) return false;
+        if (sUnitcomName != null ? !sUnitcomName.equals(that.sUnitcomName) : that.sUnitcomName != null) return false;
+        if (sUnitinformation != null ? !sUnitinformation.equals(that.sUnitinformation) : that.sUnitinformation != null)
+            return false;
+        if (sFlecontphone != null ? !sFlecontphone.equals(that.sFlecontphone) : that.sFlecontphone != null)
+            return false;
+        if (sUnitslocation != null ? !sUnitslocation.equals(that.sUnitslocation) : that.sUnitslocation != null)
+            return false;
+        if (sFlesalary != null ? !sFlesalary.equals(that.sFlesalary) : that.sFlesalary != null) return false;
+        if (sIsemployment != null ? !sIsemployment.equals(that.sIsemployment) : that.sIsemployment != null)
+            return false;
+        if (sChangedhistory != null ? !sChangedhistory.equals(that.sChangedhistory) : that.sChangedhistory != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sId != null ? sId.hashCode() : 0;
+        result = 31 * result + (sGraduationyear != null ? sGraduationyear.hashCode() : 0);
+        result = 31 * result + (sName != null ? sName.hashCode() : 0);
+        result = 31 * result + (sIdentitycard != null ? sIdentitycard.hashCode() : 0);
+        result = 31 * result + (sSex != null ? sSex.hashCode() : 0);
+        result = 31 * result + (sNation != null ? sNation.hashCode() : 0);
+        result = 31 * result + (sPoliticaloutlook != null ? sPoliticaloutlook.hashCode() : 0);
+        result = 31 * result + (sEducation != null ? sEducation.hashCode() : 0);
+        result = 31 * result + (sMajor != null ? sMajor.hashCode() : 0);
+        result = 31 * result + (sCulturemode != null ? sCulturemode.hashCode() : 0);
+        result = 31 * result + (sBirthplace != null ? sBirthplace.hashCode() : 0);
+        result = 31 * result + (sDepartment != null ? sDepartment.hashCode() : 0);
+        result = 31 * result + (sClass != null ? sClass.hashCode() : 0);
+        result = 31 * result + sStudylength;
+        result = 31 * result + (sForeignlanguages != null ? sForeignlanguages.hashCode() : 0);
+        result = 31 * result + (sGraduating != null ? sGraduating.hashCode() : 0);
+        result = 31 * result + (sSendout != null ? sSendout.hashCode() : 0);
+        result = 31 * result + (sDispatchstate != null ? sDispatchstate.hashCode() : 0);
+        result = 31 * result + (sHomephone != null ? sHomephone.hashCode() : 0);
+        result = 31 * result + (sMyphone != null ? sMyphone.hashCode() : 0);
+        result = 31 * result + (sReceivingunit != null ? sReceivingunit.hashCode() : 0);
+        result = 31 * result + (sSign != null ? sSign.hashCode() : 0);
+        result = 31 * result + (sForaddress != null ? sForaddress.hashCode() : 0);
+        result = 31 * result + (sTrandepartment != null ? sTrandepartment.hashCode() : 0);
+        result = 31 * result + (sUnittranloaction != null ? sUnittranloaction.hashCode() : 0);
+        result = 31 * result + (sReporttype != null ? sReporttype.hashCode() : 0);
+        result = 31 * result + (sUnitreceivtype != null ? sUnitreceivtype.hashCode() : 0);
+        result = 31 * result + (sGraaddress != null ? sGraaddress.hashCode() : 0);
+        result = 31 * result + (sUnitcategorycode != null ? sUnitcategorycode.hashCode() : 0);
+        result = 31 * result + (sUnitnature != null ? sUnitnature.hashCode() : 0);
+        result = 31 * result + (sUnitorgcode != null ? sUnitorgcode.hashCode() : 0);
+        result = 31 * result + (sUnittype != null ? sUnittype.hashCode() : 0);
+        result = 31 * result + (sUnitcomName != null ? sUnitcomName.hashCode() : 0);
+        result = 31 * result + (sUnitinformation != null ? sUnitinformation.hashCode() : 0);
+        result = 31 * result + (sFlecontphone != null ? sFlecontphone.hashCode() : 0);
+        result = 31 * result + (sUnitslocation != null ? sUnitslocation.hashCode() : 0);
+        result = 31 * result + (sFlesalary != null ? sFlesalary.hashCode() : 0);
+        result = 31 * result + (sIsemployment != null ? sIsemployment.hashCode() : 0);
+        result = 31 * result + sIschanged;
+        result = 31 * result + (sChangedhistory != null ? sChangedhistory.hashCode() : 0);
+        return result;
+    }
+}
